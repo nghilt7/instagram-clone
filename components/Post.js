a9bbe423ad2848dd8f6ed48afdf84a6f1cd53d4c
nghilt7 <nghilt2307@gmail.com>
2022-06-21 12:48:08 +0700

    styles caption and input comment of the post components

--- components/Post.js
+++ components/Post.js
@@ -3,6 +3,7 @@ import {
   HeartIcon,
   ChatIcon,
   BookmarkIcon,
+  EmojiHappyIcon,
 } from "@heroicons/react/outline";
 
 export default function Post({ id, username, userImg, img, caption }) {
@@ -30,6 +31,23 @@ export default function Post({ id, username, userImg, img, caption }) {
         </div>
         <BookmarkIcon className="btn" />
       </div>
+
+      {/* Post Comments */}
+      <p className="p-5 truncate">
+        <span className="font-bold mr-2">{username}</span>
+        {caption}
+      </p>
+
+      {/* Post input box */}
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          className="border-none flex-1 focus:ring-0"
+          type="text"
+          placeholder="Enter your comment..."
+        />
+        <button className="text-blue-400 font-bold">Post</button>
+      </form>
     </div>
   );
 }
