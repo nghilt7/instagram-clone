b396cd6e78220041b251058221f4e09692012dcd
nghilt7 <nghilt2307@gmail.com>
2022-06-21 19:01:57 +0700

    modify mini profile and story component

--- components/Feed.js
+++ components/Feed.js
@@ -2,10 +2,18 @@ import Stories from "./Stories";
 import Posts from "./Posts";
 import Miniprofile from "./Miniprofile";
 import Suggestions from "./Suggestions";
+import { useSession } from "next-auth/react";
 
 export default function Feed() {
+  const { data: session } = useSession();
   return (
-    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+    <main
+      className={`grid ${
+        session
+          ? "grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto"
+          : "grid-cols-1 md:grid-cols-2 md:max-w-3xl mx-auto"
+      } `}
+    >
       {/* Left */}
       <section className="md:col-span-2">
         {/* Stories */}
