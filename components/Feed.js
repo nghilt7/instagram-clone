048b8b43bbed6f0fccdd1d5410f12f24def7f8e2
nghilt7 <nghilt2307@gmail.com>
2022-06-21 10:04:46 +0700

    fake data using minifaker

---
+++ components/Feed.js
@@ -0,0 +1,22 @@
+import Stories from "./Stories";
+
+export default function Feed() {
+  return (
+    <main>
+      {/* Left */}
+      <section>
+        {/* Stories */}
+        <Stories />
+        {/* Posts */}
+      </section>
+
+      {/* Right */}
+
+      <section>
+        {/* Mini profile */}
+
+        {/* Suggections */}
+      </section>
+    </main>
+  );
+}
